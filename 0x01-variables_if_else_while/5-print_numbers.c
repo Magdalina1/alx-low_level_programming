ca7d9a75a9920f66ee7eb311358a81da2bdd18b4
Magdalina1 <majdalol95@gnail.com>
2023-03-17 12:04:52 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in new line.
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+		printf("%i", n);
+	}
+
+	putchar('\n');
+	return (0);
+}
