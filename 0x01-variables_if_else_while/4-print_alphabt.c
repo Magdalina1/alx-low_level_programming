bdedc71fdf8d051b934272dbe09fd0ccf45e1f03
Magdalina1 <majdalol95@gnail.com>
2023-03-17 11:52:47 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdoi.h>
+
+/**
+ * main - print alphabet in new line.
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
