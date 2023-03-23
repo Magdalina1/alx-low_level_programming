b5034216f599c957efef2a205b18eaf159202384
Magdalina1 <majdalol95@gnail.com>
2023-03-23 07:29:01 -0700

    6

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,7 +8,7 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-oid print_line(int n);
+void print_line(int n);
 void print_diagonal(int n);
 #endif
 
