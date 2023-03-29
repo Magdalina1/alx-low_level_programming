8af29b8e5c4a36884f3af9fe67739f080134f424
Magdalina1 <majdalol95@gnail.com>
2023-03-29 13:38:48 -0700

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,16 +2,15 @@
 
 /**
  * string_toupper - that changes all lowercase letters.
- * @:
- * @str:
+ * @str: string to be changing.
  * Return: str
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
