b457b1f487b026fd2fdb73d405e641d24b2bc9a7
Magdalina1 <majdalol95@gmail.com>
2023-04-03 05:45:51 -0700

    0

---
+++ 0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - that fills memory with a constant byte.
+ * @s: the size of memory to print.
+ * @b: the adress of memory to print.
+ * @n: the int
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	ungigned int n;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
