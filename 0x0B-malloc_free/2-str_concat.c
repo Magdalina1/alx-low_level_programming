701323bacf0959f697d20418cf03ffda3eb0dfe0
Magdalina1 <majdalol95@gmail.com>
2023-04-11 14:08:19 -0700

    fixing 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,23 +13,26 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *result = malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
+	char *concat_str;
+	int i, j;
+	size_t len1, len2;
 
 	if (s1 == NULL)
-	{
-		s1 = ("");
-	}
+		s1 = "";
 	if (s2 == NULL)
-	{
-		s2 = ("");
-	}
+		s2 = "";
 
-	if (result == NULL)
-	{
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	concat_str = malloc((len1 + len2 + 1) * sizeof(char));
+	if (concat_str == NULL)
 		return (NULL);
-	}
-	strcpy(result, s1);
-	strcat(result, s2);
+	for (i = 0; s1[i] != '\0'; i++)
+		concat_str[i] = s1[i];
+	for (j = 0; s2[j] != '\0'; j++)
+		concat_str[i + j] = s2[j];
 
-	return (result);
+	concat_str[i + j] = '\0';
+	return (concat_str);
 }
