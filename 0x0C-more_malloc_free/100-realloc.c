008660efb0386d99ebc92f40d1086d105c6866d4
Magdalina1 <majdalol95@gmail.com>
2023-04-13 17:06:18 -0700

    4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,6 +29,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
+	if (new_size == old_size)
+		return (ptr);
 
 	min_size = old_size < new_size ? old_size : new_size;
 	memcpy(new_ptr, ptr, min_size);
