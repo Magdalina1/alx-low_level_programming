e33d1d4b238b1e51e1e177d5d4ca6bdaf443437c
Magdalina1 <majdalol95@gmail.com>
2023-04-17 11:38:06 -0700

    2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints the name of the file it was compiled from new line.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char filename[256];
+
+	if (getenv("FILENAME") != NULL)
+	{
+		strcpy(filename, getenv("FILENAME"));
+	}
+	printf("%s\n", filename);
+	return (0);
+}
