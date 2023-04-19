079fea5f3b77992a7d48a8b8b2091b9464e46a36
Magdalina1 <majdalol95@gmail.com>
2023-04-19 02:53:30 -0700

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * print_name - prints a name.
+ * @name: name of person
+ * @f: fonction
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
