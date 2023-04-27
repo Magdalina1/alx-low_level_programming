d056c1e95a6ce5d133a365fc2660f763c34a2f33
Magdalina1 <majdalol95@gmail.com>
2023-04-27 08:26:06 -0700

    fix 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,6 +13,8 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	int len = 0;
+
 
 	if (head == NULL || str == NULL)
 	{
@@ -26,6 +28,11 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
 	new_node->str = strdup(str);
 
 	if (new_node->str == NULL)
@@ -34,6 +41,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
+	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
 
