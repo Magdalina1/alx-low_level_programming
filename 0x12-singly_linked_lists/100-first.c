601d19fb1a482a16c8da74d330c78aada4dcff6c
Magdalina1 <majdalol95@gmail.com>
2023-04-27 16:12:45 -0700

    fix 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * print_statement - prints You're beat! and yet, you must allow,
+ * print_before_main - prints You're beat! and yet, you must allow,
  * \nI bore my house upon my back!\n.
  */
 
-void __attribute__ ((constructor)) print_statement()
+void __attribute__ ((constructor)) print_before_main()
 {
 	printf("You're beat! and yet , you must allow\n");
 	printf("I bore my house upon my back!\n");
