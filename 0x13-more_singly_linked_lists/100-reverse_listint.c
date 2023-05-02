80c8994bab4b5c85cdc6cbdac47fb42fb1b3fcd7
Magdalina1 <majdalol95@gmail.com>
2023-05-02 14:50:01 -0700

    fix 11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,7 +11,7 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *prev_node;
 	listint_t *next_node;
 
-	if (!head || !*head || !(*head)->next)
+	if (!head || !*head)
 		return (NULL);
 
 	prev_node = NULL;
