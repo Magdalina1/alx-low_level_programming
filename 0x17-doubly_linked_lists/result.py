cf2062fac14461ff09789763b2b42aafd619f349
Magdalina1 <majdalol95@gmail.com>
2023-06-15 13:55:43 -0700

    102-result

--- 0x17-doubly_linked_lists/result.py
+++ 0x17-doubly_linked_lists/result.py
@@ -1,13 +1,31 @@
+# Define a function to check if a number is a palindrome
 def is_palindrome(n):
-    return str(n) == str(n)[::-1]
+  # Convert the number to a string
+  s = str(n)
+  # Compare the string with its reverse
+  return s == s[::-1]
 
-max_palindrome = 0
+# Initialize the largest palindrome and the factors
+largest = 0
+factor1 = 0
+factor2 = 0
 
+# Loop through all possible pairs of three-digit numbers
 for i in range(100, 1000):
-    for j in range(100, 1000):
-        product = i * j
-        if is_palindrome(product) and product > max_palindrome:
-            max_palindrome = product
+  for j in range(i, 1000):
+    # Calculate the product
+    product = i * j
+    # Check if it is a palindrome and larger than the current largest
+    if is_palindrome(product) and product > largest:
+      # Update the largest and the factors
+      largest = product
+      factor1 = i
+      factor2 = j
 
-with open('102-result', 'w') as file:
-    file.write(str(max_palindrome))
+# Print the result and the factors
+print(largest)
+print(factor1, factor2)
+
+# Save the result in the file 102-result
+with open("102-result", "w") as f:
+  f.write(str(largest))
