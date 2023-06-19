1656159b09cfcfe6349746490588c30ec3a7542f
Magdalina1 <majdalol95@gmail.com>
2023-06-19 12:53:23 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
-i#!/bin/bash
+#!/bin/bash
 wget -P .. https://raw.githubusercontent.com/Magdalina1/alx-low_level_programming/master/0x18-dynamic_libraries/libcust.so
 export LD_PRELOAD="$PWD/../libcust.so"
