070368b8a550f72bf3d337d38c79e464fc69f599
Magalina1 <majdalol95@gmail.com>
2023-08-23 14:35:22 +0300

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
+"""Island Perimeter"""
+
 
 def island_perimeter(grid):
-"""Function to calculate the perimeter of the island in given grid."""
-
-
-	perimeter = 0
-
-	# Iterate through each cell in the grid
-	for i in range(len(grid)):
-		for j in range(len(grid[0])):
-
-			#whenever we encounter a land cell
-			cell
-            		if grid[i][j] == 1:
-                		perimeter += 4
-                		# The cell loses one edge if there is a neighboring land cell on the right or bottom
-                		if i < (len(grid) - 1): 
-                    			if grid[i+1][j] == 1: 
-                        			perimeter -= 2
-                		if j < (len(grid[0]) - 1): 
-                    			if grid[i][j+1] == 1: 
-                        			perimeter -= 2
+    """returns the perimeter of the island"""
+    patches = 0
+    borders = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                patches += 1
+                if row != len(grid) - 1:
+                    if grid[row + 1][col] == 1:
+                        borders += 1
+                if col != len(grid[row]) - 1:
+                    if grid[row][col + 1] == 1:
+                        borders += 1
+    perimeter = (patches * 4) - (borders * 2)
     return perimeter
-
