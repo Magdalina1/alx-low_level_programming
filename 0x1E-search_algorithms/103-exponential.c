d8b0d8ef10e1ac387e047a852342f5dfb0107166
Magdalina1 <majdalol95@gmail.com>
2023-10-11 08:26:55 -0700

    103-exponential.c

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -50,5 +50,36 @@ int binary_search_helper(int *array, int value,
 			return ((int)mid);
 	}
 
-	return (-1):
+	return (-1);
+}
+
+/**
+ * exponential_search - searches for a value in a sorted array of integers
+ * using an exponential search algorithm
+ * @array: pointer to first element of array to search
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: first index containing `value`, or -1 if `value` not found or
+ * `array` is NULL
+ */
+
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t left, right, bound = 1;
+
+	if (!array || size == 0)
+		return (-1);
+
+	while (bound < size && array[bound] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n",
+		       bound, array[bound]);
+		bound *= 2;
+	}
+
+	left = bound / 2;
+	right = min(bound, size - 1);
+	/* 'found' message generated even if array[right] < value */
+	printf("Value found between indexes [%lu] and [%lu]\n", left, right);
+	return (binary_search_helper(array, value, left, right));
 }
